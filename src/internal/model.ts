276352cbbfdfa16346d29b0b5b2d6fb27f9905bf
Stephen Lautier <stephen.lautier@outlook.com>
2020-10-19 23:49:05 +0200

    eslint init

--- src/internal/model.ts
+++ src/internal/model.ts
@@ -1,8 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export interface Dictionary<T = any> {
 	[key: string]: T;
 }
 
 export interface PackageFormat {
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	[key: string]: any;
 
 	name: string;
--- src/publish.ts
+++ src/publish.ts
@@ -14,7 +14,7 @@ import chalk from "chalk";
 export async function publisher($args: {
 	publishFn?: (bump: string) => Promise<void>,
 	bump: string
-}) {
+}): Promise<void> {
 	const args = { publishFn: performGulpPublish, ...$args };
 	const git = simpleGit();
 
